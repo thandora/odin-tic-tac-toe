7abff62fc5fa6e2b15d4b7331753671cde4e7c38
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-12-02 14:02:21 +0800

    Add function to handle checking for winning board

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Variables for testing.
-const DEBUG_NTURNS = 3;
+const DEBUG_NTURNS = 5;
 
 const gameBoard = (function () {
-  const board = [
+  let board = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
@@ -16,12 +16,28 @@ const gameBoard = (function () {
   const cellMarked = function (coordinates) {
     // Check if the `coordinates` point to a marked cell (unmarked cells are NULL).
     // Returns a boolean. `true` if marked `false` otherwise.
-
     const [x, y] = coordinates;
     return board[x][y] === !null;
   };
 
-  return { board, mark, cellMarked };
+  const getBoard = function () {
+    return board;
+  };
+
+  const getCell = function (coordinates) {
+    const [x, y] = coordinates;
+    return board[x][y];
+  };
+
+  const resetBoard = function () {
+    board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+  };
+
+  return { getBoard, mark, cellMarked, getCell, resetBoard };
 })();
 
 function createPlayer(name, markSymbol) {
@@ -51,7 +67,8 @@ const gameflow = function (playerA, playerB) {
     const currentPlayer = players[currentPlayerIndex];
     console.log(`${currentPlayer.name}'s turn (${currentPlayer.markSymbol})`);
     // Transform "XY" to [X int, Y int]
-    const coordinates = askUserCoordinates(); // -1 is for "real" coordinates
+    const coordinates = askUserCoordinates();
+    // -1 is for "real" coordinates
     // since input is assuming indexed 1. (first cell is at (1, 1))
 
     if (gameBoard.cellMarked(coordinates)) {
@@ -60,17 +77,79 @@ const gameflow = function (playerA, playerB) {
       gameBoard.mark(coordinates, currentPlayer.markSymbol);
     }
 
+    console.log(checkForWinner(gameBoard));
     currentPlayerIndex = currentPlayerIndex == 1 ? 0 : 1;
     x++;
     console.log(gameBoard.board);
   } while (x < DEBUG_NTURNS);
 };
 
+function checkForWinner(gameboard) {
+  const winCombinations = [
+    // Horizontal combinations
+    [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ],
+    [
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ],
+    // Vertical combinations
+    [
+      [0, 0],
+      [1, 0],
+      [2, 0],
+    ],
+    [
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ],
+    [
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ],
+    // Diagonal
+    [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ],
+    [
+      [2, 0],
+      [1, 1],
+      [0, 2],
+    ],
+  ];
+
+  for (let i = 0; i < winCombinations.length; i++) {
+    const cellA = gameboard.getCell(winCombinations[i][0]);
+    const cellB = gameboard.getCell(winCombinations[i][1]);
+    const cellC = gameboard.getCell(winCombinations[i][2]);
+
+    if (cellA === cellB && cellA === cellC && cellA !== null) {
+      return true;
+    }
+  }
+  return false;
+}
+
 // Helper functions
 function askUserCoordinates() {
   return prompt("Coordinates: (1, 1) through (3, 3)")
     .split("")
     .map((x) => +x - 1);
+  // -1 is for "real" coordinates
+  // since input is assumed to be 1-indexed. (first cell is at (1, 1))
 }
 
 const p1 = createPlayer("Alice", "X");
